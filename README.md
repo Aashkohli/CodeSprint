16261e63349fd77683baa51987f926ec341e4418
Aashvik Kohli <76828448+Aashkohli@users.noreply.github.com>
2023-11-06 22:40:59 -0800

    update

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ For Tinovation CodeSprint
 Testing - Ayush
 
 Testing - Ashaaz
+
+we should really make this file look good
+like gg's level good
+like really good
+like lebron messi typa beat
--- main.py
+++ main.py
@@ -2,18 +2,16 @@ import pygame
 
 pygame.init()
 
+run = True
+
 display = pygame.display.set_mode((1000,600))
 clock = pygame.time.Clock()
-
 FPS = 30
-def game():
-    while True: 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        pygame.display.update()
-        clock.tick(FPS)
-
-game()
+while run: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    pygame.display.update()
+    clock.tick(FPS)
+    
 pygame.quit()
-
